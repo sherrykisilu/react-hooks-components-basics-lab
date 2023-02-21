4474d00ad5cfc7f24358b7c65b06405fb575ea51
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-02-21 16:19:51 +0300

    done again

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,8 @@
 import React from "react";
+import NavBar from "./NavBar";
+import About from "./About";
+import Home from "./Home";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
-
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
-
-function About(){
-  <div id="about">
-    <h2>"About"</h2>
-  </div>
-}
 
 function App() {
   return (
@@ -31,5 +13,5 @@ function App() {
     </div>
   );
 }
-export default Home;
+export default App;
 
